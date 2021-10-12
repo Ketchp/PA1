fe35ee2a0972e8b8fd9edfff03f7fe4332178c68
slavkste <slavkste@fel.cvut.cz>
2021-10-12 09:50:29 +0200

    cv04: solved all edge cases

--- cv04/main.c
+++ cv04/main.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <math.h>
+#include <float.h>
+
+int precision = 1e3;
 
 int charInStr(char character, char* str);
+int LDBLequal(long double a, long double b);
+//int DBLequal(double a, double b);
 
 int main(){
     printf("Zadejte rovnici:\n");
-    long double a,b,c,expected=0;
+    long double A,B,C,expected=0;
     char operand, equal;
-    int ret = scanf(" %LF %c %LF %c %LF", &a, &operand, &b, &equal, &c);
-    if(ret != 5 || equal != '=' || !charInStr(operand, (char *)"+-*/") || (operand == '/' && b==0)){
+    int ret = scanf(" %LF %c %LF %c %LF", &A, &operand, &B, &equal, &C);
+//    printf("%d, %Lg, %Lg, %Lg, %c, %c\n", ret, A, B, C, operand, equal);
+    if(!LDBLequal(A,(double)A) || !LDBLequal(B,(double)B) || !LDBLequal(C,(double)C)){
         printf("Nespravny vstup.\n");
         return 0;
     }
-    if(a == NAN || b == NAN || c == NAN || a == INFINITY || b == INFINITY || c == INFINITY ||
-    a == -INFINITY || b == -INFINITY || c == -INFINITY){
+    if(ret != 5 || equal != '=' || !charInStr(operand, (char *)"+-*/") || (operand == '/' && LDBLequal(B,0))){
+        printf("Nespravny vstup.\n");
+        return 0;
+    }
+    if(A == NAN || B == NAN || C == NAN || A == INFINITY || B == INFINITY || C == INFINITY ||
+    A == -INFINITY || B == -INFINITY || C == -INFINITY){
         printf("Nespravny vstup.\n");
         return 0;
     }
@@ -25,25 +35,25 @@ int main(){
     }
     switch (operand){
     case '+':
-        expected = a+b;
+        expected = A+B;
         break;
     case '-':
-        expected = a-b;
+        expected = A-B;
         break;
     case '*':
-        expected = a*b;
+        expected = A*B;
         break;
     case '/':
-        expected = (long long)(a/b);
+        expected = truncl(A/B);
         break;
     
     }
-    if(expected == INFINITY || expected == NAN){
+    if(expected == INFINITY || expected == NAN || expected == -INFINITY){
          printf("Nespravny vstup.\n");
         return 0;       
     }
-    if(abs(expected - c) < (long double)1/100000000000) printf("Rovnice je spravne.\n");
-    else printf("%Lg != %Lg\n", expected, c);
+    if(LDBLequal(expected, C)) printf("Rovnice je spravne.\n");
+    else printf("%Lg != %Lg\n", expected, C);
     return 0;
 }
 
@@ -51,3 +61,11 @@ int charInStr(char character, char* str){
     for(int i = 0; str[i] != '\0'; i++) if(str[i] == character) return 1;
     return 0;
 }
+
+int LDBLequal(long double a, long double b){
+    return abs(a-b) <= precision*LDBL_EPSILON*abs(a+b);
+}
+/*
+int DBLequal(double a, double b){
+    return abs(a-b) <= precision*DBL_EPSILON*abs(a+b);
+}*/
