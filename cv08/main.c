d488b180d07bd3e250c28af4e30a845ea69514c9
slavkste <slavkste@fel.cvut.cz>
2021-11-08 18:59:26 +0100

    cv08: fixed char overflow

--- cv08/main.c
+++ cv08/main.c
@@ -4,15 +4,16 @@
 
 typedef struct vector
 {
-    char *elements;
+    int *elements;
     int size, capacity;
 } vector_t;
 
 vector_t newVector();
-void push( vector_t *vect, char element);
+void push( vector_t *vect, int element);
 void increment( vector_t *vect, int position);
-void insert( vector_t *vect, char element, int position);
-char get( vector_t *vect, int position);
+void insert( vector_t *vect, int element, int position);
+int get( vector_t *vect, int position);
+void freeVector( vector_t *vect );
 
 int main()
 {
@@ -39,6 +40,8 @@ int main()
                 if((width == current_width) == (width == 0))
                 {
                     printf("Nespravny vstup.\n");
+                    freeVector( &grid );
+                    freeVector( &mines );
                     return 0;
                 }
                 width = current_width;
@@ -47,14 +50,36 @@ int main()
                 break;        
             default:
                 printf("Nespravny vstup.\n");
+                freeVector( &grid );
+                freeVector( &mines );
                 return 0;
         }
     }
     if(current_width != 0 || height == 0)
     {
         printf("Nespravny vstup.\n");
+        freeVector( &grid );
+        freeVector( &mines );
         return 0;
     }
+
+/*
+    for(int mine = 0; mine < mines.size; mine++)
+    {
+        printf("%d ", mines.elements[mine]);
+    }
+
+    printf("\n%dx%d\n", width, height); 
+    for(int h = 0; h < height; h++)
+    {
+        for(int w = 0; w < width; w++)
+        {
+            int tile = get( &grid, w + h * width );
+            printf( "%d", tile );
+        }
+        printf( "\n" );
+    }
+*/
 
     for(int mine = 0; mine < mines.size; mine++)
     {
@@ -63,11 +88,10 @@ int main()
 //        printf("%d %d\n", mine_w, mine_h);
         for(int dw = -1; dw <= 1; dw++)
         {
-            if( (mine_w + dw + width) % width != mine_w + dw ) continue;
+            if( ( mine_w + dw ) < 0 || ( mine_w + dw ) >= width ) continue;
             for(int dh = -1; dh <= 1; dh++)
             {
-                if( dw == dh && dh == 0) continue;
-                if( (mine_h + dh + height) % height != mine_h + dh ) continue;
+                if( ( mine_h + dh ) < 0 || ( mine_h + dh ) >= height ) continue;
                 increment( &grid, ( mine_w + dw ) + ( mine_h + dh ) * width );
             }
         }
@@ -78,28 +102,30 @@ int main()
     {
         for(int w = 0; w < width; w++)
         {
-            char tile = get( &grid, w + h * width );
+            int tile = get( &grid, w + h * width );
             if(!tile) printf( "." );
             else if(tile > 8) printf( "*" );
             else printf( "%d", tile );
         }
         printf( "\n" );
     }
+    freeVector( &grid );
+    freeVector( &mines );
     return 0;
 }
 
 vector_t newVector()
 {
-    vector_t vect = {(char *)malloc(sizeof(char)), 0, 1};
+    vector_t vect = {(int *)malloc(sizeof(int)), 0, 1};
     return vect;
 }
 
-void insert( vector_t *vect, char element, int position)
+void insert( vector_t *vect, int element, int position)
 {
     while( position >= vect->capacity )
     {
         vect->capacity <<= 1;
-        vect->elements = (char *)realloc( vect->elements, vect->capacity * sizeof(char) );
+        vect->elements = (int *)realloc( vect->elements, vect->capacity * sizeof(int) );
     }
     vect->elements[position] = element;
 }
@@ -109,14 +135,19 @@ void increment( vector_t *vect, int position)
     vect->elements[position]++;
 }
 
-void push( vector_t *vect, char element)
+void push( vector_t *vect, int element)
 {
     insert( vect, element, vect->size );
     vect->size++;
 }
 
-char get( vector_t *vect, int position)
+int get( vector_t *vect, int position)
 {
     assert( position < vect->capacity );
     return vect->elements[position];
+}
+
+void freeVector( vector_t *vect)
+{
+    free(vect->elements);
 }
