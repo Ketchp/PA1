0b47da001f5142867fea14d90bb829267181ef44
slavkste <slavkste@fel.cvut.cz>
2021-11-16 07:38:26 +0100

    hw05: main functions

--- hw05/main.c
+++ hw05/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define PRINTABLE_CHARS ('~' - '!')
+#define SMALLEST_CHAR '!'
+#define LARGEST_CHAR '~'
+#define PRINTABLE_CHARS (LARGEST_CHAR - SMALLEST_CHAR)
 #define MAX_ITEM_LEN 100
 #define PARSE_STRING "%99s"
 
@@ -15,21 +18,29 @@ struct leaf
     leaf_t *root;
     leaf_t *kids[ PRINTABLE_CHARS ];
     link_t *reference;
-    char leaf_name;
+    char letter;
+    int count;
 };
 
 struct link
 {
     link_t *next, *previous;
     leaf_t *name;
+    item_t *root;
 };
 
 struct item
 {
-    link_t *first, *last;
+    link_t *first_link;
     item_t *next, *previous;
     int count;
+    int value;
 };
+
+int treeIdx(char c);
+void add(leaf_t *stringTree, char *new_string, item_t *topSellers,
+            item_t *last_item, int geItemCount, int count, item_t *list_end);
+leaf_t *getLeaf( leaf_t *stringTree, char *new_string);
 
 int main()
 {
@@ -40,9 +51,20 @@ int main()
     {
         printf( "Nespravny vstup.\n" );
     }
-    item_t items[ count ];
     printf( "Pozadavky:\n" );
     char new_item[ MAX_ITEM_LEN ];
+    leaf_t *stringTree = (leaf_t *)calloc( 1, sizeof(*stringTree) );
+    item_t *topSellers = (item_t *)calloc( count, sizeof(*topSellers) );
+    topSellers[0].next = topSellers + 1;
+    for(int i = 1; i < count-1; i++)
+    {
+        topSellers[i].next = topSellers + i + 1;
+        topSellers[i].previous = topSellers + i - 1;
+    }
+    topSellers[count - 1].previous = topSellers + count - 2;
+    item_t *last_item = topSellers;
+    item_t *list_end = topSellers + count - 1;
+    int geItemCount = 0;
     while( scanf( "%c", &operation ) != EOF )
     {
         switch( operation )
@@ -53,7 +75,8 @@ int main()
                 printf( "Nespravny vstup.\n" );
                 return 0;
             }
-            //todo
+            add(stringTree, new_item, topSellers,
+                 last_item, geItemCount, count, list_end);
             break;
 
         case '#':
@@ -78,4 +101,66 @@ int main()
         }
     }
     return 0;
+}
+
+void add(leaf_t *stringTree, char *new_string, item_t *topSellers,
+ item_t *last_item, int geItemCount, int count, item_t *list_end)
+{
+    leaf_t *item_leaf = getLeaf( stringTree, new_string );
+    item_leaf->count++;
+    if( last_item->value <= item_leaf->count )
+    {
+        link_t *reference = item_leaf->reference;
+        if( reference )
+        {
+            shiftLink(reference);
+        }
+    }
+}
+
+leaf_t *getLeaf( leaf_t *stringTree, char *new_string)
+{
+    for(char *c = new_string; *new_string; new_string++)
+    {
+        if( !stringTree->kids[ treeIdx(*c) ] )
+        {
+            stringTree->kids[ treeIdx(*c) ] = 
+                (leaf_t *)calloc( 1, sizeof(*stringTree) );
+            stringTree->kids[ treeIdx(*c) ].root = stringTree;
+        }
+        stringTree = stringTree[ treeIdx(*c) ];
+        stringTree->letter = *c;
+    }
+    return stringTree;
+}
+
+int treeIdx(char c)
+{
+    return c - SMALLEST_CHAR;
+}
+
+shiftLink(link_t *reference)
+{
+    if( reference->next )
+    {
+        reference->next->previous = reference->previous;
+    }
+    if( reference->previous )
+    {
+        reference->previous->next = reference->next;
+    }
+    if( !reference->previous && !reference->next &&
+        ( !reference->root->previous ||
+            reference->root->previous->value == item_leaf->count ) )
+    {
+        if( reference->root->next )
+        {
+            reference->root->next->previous = reference->previous;
+        }
+        if( reference->root->previous )
+        {
+            reference->root->previous->next = 
+        }
+    }
+
 }
