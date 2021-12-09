5e29c4614fa62e6791f1f4547dc29474dcdf9d24
slavkste <slavkste@fel.cvut.cz>
2021-12-09 22:05:38 +0100

    cv12: basic setup

---
+++ cv12/testI.c
@@ -0,0 +1,243 @@
+#ifndef __PROGTEST__
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct TItem
+{
+  struct TItem * m_Next;
+  int      m_Val;
+} TITEM;
+
+typedef struct TData
+{
+  TITEM * m_First;
+  TITEM * m_Last;
+} TDATA;
+
+#endif /* __PROGTEST__ */
+
+void insertStart ( TDATA * l, int x )
+{
+  TITEM *newItem = (TITEM *)malloc( sizeof( *newItem ) );
+  newItem->m_Val = x;
+  newItem->m_Next = l->m_First;
+  l->m_First = newItem;
+  if(!l->m_Last) l->m_Last = newItem;
+}
+
+void insertEnd   ( TDATA * l, int x )
+{
+  TITEM *newItem = (TITEM *)malloc( sizeof( *newItem ) );
+  newItem->m_Val = x;
+  newItem->m_Next = NULL;
+  if( !l->m_Last )
+  {
+    l->m_First = newItem;
+    l->m_Last = newItem;
+    return;
+  }
+  l->m_Last->m_Next = newItem;
+  l->m_Last = newItem;
+}
+
+int  findMax     ( TDATA * l )
+{
+  int max = -__INT_MAX__ - 1;
+  TITEM *item = l->m_First;
+  while( item )
+  {
+    max = item->m_Val > max ? item->m_Val : max;
+    item = item->m_Next;
+  }
+  return max;
+}
+
+int  removeMax   ( TDATA * l )
+{
+  int max = findMax( l );
+  int removed = 0;
+  TITEM *item = l->m_First;
+  while( item && item->m_Val == max )
+  {
+    TITEM *tmp = item->m_Next;
+    free( item );
+    item = tmp;
+    removed++;
+  }
+  l->m_First = item;
+  if( !item )
+  {
+    l->m_Last = NULL;
+    return removed;
+  }
+  while( item->m_Next )
+  {
+    if( item->m_Next->m_Val == max )
+    {
+      TITEM *tmp = item->m_Next;
+      item->m_Next = item->m_Next->m_Next;
+      free( tmp );
+      removed++;
+      continue;
+    }
+    item = item->m_Next;
+  }
+  l->m_Last = item;
+  return removed;
+}
+
+void destroyAll  ( TDATA * l )
+{
+  TITEM *item = l->m_First;
+  while ( item )
+  {
+    TITEM *tmp = item->m_Next;
+    free( item );
+    item = tmp;
+  }
+  
+}
+
+#ifndef __PROGTEST__
+
+void printList( TDATA *l )
+{
+  TITEM *item = l->m_First;
+  printf("List: ");
+  while( item )
+  {
+    printf( "%d -> ", item->m_Val );
+    item = item->m_Next;
+  }
+  printf( "%p \n", (void *)item );
+}
+
+int main ( void )
+{
+  TDATA a;
+  a . m_First = a . m_Last = NULL;
+  insertEnd ( &a, 1 );
+  insertEnd ( &a, 2 );
+  insertEnd ( &a, 1 );
+  insertEnd ( &a, 3 );
+  insertEnd ( &a, 4 );
+  insertEnd ( &a, 2 );
+  insertEnd ( &a, 5 );
+  assert ( a . m_First != NULL
+           && a . m_First -> m_Val == 1
+           && a . m_First -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Val == 2
+           && a . m_First -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Val == 1
+           && a . m_First -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Val == 3
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Val == 4
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Val == 2
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Val == 5
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next == NULL
+           && a . m_Last == a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next );
+  assert ( removeMax ( &a ) == 1 );
+  assert ( a . m_First != NULL
+           && a . m_First -> m_Val == 1
+           && a . m_First -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Val == 2
+           && a . m_First -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Val == 1
+           && a . m_First -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Val == 3
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Val == 4
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Val == 2
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next == NULL
+           && a . m_Last == a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next );
+  destroyAll ( &a );
+
+  a . m_First = a . m_Last = NULL;
+  insertEnd ( &a, 1 );
+  insertEnd ( &a, 2 );
+  insertEnd ( &a, 3 );
+  insertEnd ( &a, 0 );
+  insertEnd ( &a, 2 );
+  insertEnd ( &a, 3 );
+  insertStart ( &a, 1 );
+  insertStart ( &a, 2 );
+  insertStart ( &a, 3 );
+  assert ( a . m_First != NULL
+           && a . m_First -> m_Val == 3
+           && a . m_First -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Val == 2
+           && a . m_First -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Val == 1
+           && a . m_First -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Val == 1
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Val == 2
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Val == 3
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Val == 0
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Val == 2
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Val == 3
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next == NULL
+           && a . m_Last == a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next );
+  printList( &a );
+  int count = removeMax ( &a );
+  printList( &a );
+  assert ( count == 3 );
+  assert ( a . m_First != NULL
+           && a . m_First -> m_Val == 2
+           && a . m_First -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Val == 1
+           && a . m_First -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Val == 1
+           && a . m_First -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Val == 2
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Val == 0
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Val == 2
+           && a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next == NULL
+           && a . m_Last == a . m_First -> m_Next -> m_Next -> m_Next -> m_Next -> m_Next );
+  assert ( removeMax ( &a ) == 3 );
+  assert ( a . m_First != NULL
+           && a . m_First -> m_Val == 1
+           && a . m_First -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Val == 1
+           && a . m_First -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Val == 0
+           && a . m_First -> m_Next -> m_Next -> m_Next == NULL
+           && a . m_Last == a . m_First -> m_Next -> m_Next );
+  assert ( removeMax ( &a ) == 2 );
+  assert ( a . m_First != NULL
+           && a . m_First -> m_Val == 0
+           && a . m_First -> m_Next == NULL
+           && a . m_Last == a . m_First );
+  destroyAll ( &a );
+
+  a . m_First = a . m_Last = NULL;
+  insertEnd ( &a, -1 );
+  insertEnd ( &a, -2 );
+  insertEnd ( &a, -10000 );
+  insertEnd ( &a, -1 );
+  insertEnd ( &a, -300 );
+  assert ( removeMax ( &a ) == 2 );
+  assert ( a . m_First != NULL
+           && a . m_First -> m_Val == -2
+           && a . m_First -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Val == -10000
+           && a . m_First -> m_Next -> m_Next != NULL
+           && a . m_First -> m_Next -> m_Next -> m_Val == -300
+           && a . m_First -> m_Next -> m_Next -> m_Next == NULL
+           && a . m_Last == a . m_First -> m_Next -> m_Next );
+  destroyAll ( &a );
+
+  return 0;
+}
+#endif /* __PROGTEST__ */
